170a3cedd6c04ac596865b2c12bb61484835425f
Berniece Buttermilk <bbutterm@mi-s3.21-school.ru>
2020-03-12 22:06:34 +0300

    day2"

--- corewar/visual.py
+++ corewar/visual.py
@@ -3,6 +3,7 @@ import os
 import random
 import time
 from tkinter import Tk, Canvas, Frame, BOTH
+#max y = 63 max x = 63 max coord = 3969
 lifes = 4096
 count = 4
 class champion:
@@ -19,8 +20,7 @@ class champion:
     self.y = s[1]
     self.color = choosecolor(num)
   def move(self,steps):
-    point = self.x * 64 + self.y * 64 + steps
-    new_coords = recost(point)
+    new_coords = recost(steps)
     self.x = new_coords[0]
     self.y = new_coords[1]
   def info(self):
@@ -30,7 +30,7 @@ class champion:
   def fill(self):
     global lifes
     lifes-=1
-    #addsquare(self.x,self.y,color = self.color)
+    addsquare(self.x,self.y,color = self.color)
 def initchamps(count):
   count = count-1
   champs = []
@@ -52,17 +52,17 @@ def choosecolor(num):
     color.append('yellow')
   return color
 def recost(coords):
-  ret = []
-  x = round(coords%64)
-  y = round(coords/64)
-  ret.append(x)
-  ret.append(y)
-  return ret
-champs = initchamps(count)
-champs[0].info()
-champs[1].info()
-champs[2].info()
-champs[3].info()
+    ret = []
+    x = coords%64*12
+    y = coords//64*12
+    print(round(4065 // 64))
+#    if (x != 0):
+#        x -= 1
+#    if (y != 0):
+#       y -= 1
+    ret.append(x)
+    ret.append(y)
+    return ret
 def addsquare(x, y, color):
     x = x+128
     y = y+128
@@ -79,15 +79,14 @@ img = PhotoImage(file = "Unknown1.png")
 canvas.create_image(511, 510,image = img,anchor = CENTER)
 y = 0
 x = 0
-while (x < 769):
-    color = random.choice(['red', 'blue'])
-    addsquare(x,y,color)
-    x+=12
-    if x == 768:
-        x = 0
-        y += 12
-    if y == 768:
-        time.sleep(3000)
-        break
+champs = initchamps(count)
+champs[0].fill()
+champs[0].move(64)
+champs[0].info()
+champs[0].fill()
+champs[0].move(1024)
+champs[0].info()
+champs[0].fill()
+time.sleep(3000)
 root.mainloop()
 
